b705ae748ff438a0936fd3a47d8fe060eb8c89d0
DaHaoTran <nhao4771@gmail.com>
2024-07-17 12:25:22 +0700

    Done CombosAPIController

---
+++ API/Controllers/CombosAPIController.cs
@@ -0,0 +1,145 @@
+﻿using API.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using Models;
+
+namespace API.Controllers
+{
+    [Route("api/combos")]
+    [ApiController]
+    public class CombosAPIController : ControllerBase
+    {
+        private readonly IAddable<Combo> _addsvc;
+        private readonly IEditable<Combo> _editsvc;
+        private readonly IReadable<Combo> _readsvc;
+        private readonly IDeletable<Guid, Combo> _deletesvc;
+        private readonly ILookupSvc<Guid, Combo> _lookupsvc;
+        private readonly ILookupMoreSvc<string, Combo> _lookupsvc2;
+
+        public CombosAPIController(IAddable<Combo> addsvc, 
+            IEditable<Combo> editsvc, 
+            IReadable<Combo> readsvc, 
+            IDeletable<Guid, Combo> deletesvc, 
+            ILookupSvc<Guid, Combo> lookupsvc, 
+            ILookupMoreSvc<string, Combo> lookupsvc2)
+        {
+            _addsvc = addsvc;
+            _editsvc = editsvc;
+            _readsvc = readsvc;
+            _deletesvc = deletesvc;
+            _lookupsvc = lookupsvc;
+            _lookupsvc2 = lookupsvc2;
+        }
+
+        /// <summary>
+        /// Lấy danh sách combo
+        /// </summary>
+        /// <response name="404">Không tìm thấy</response>
+        /// <response name="200">Tìm thấy</response>
+        /// <returns>Danh sách combo</returns>
+        [HttpGet]
+        public async Task<IEnumerable<Combo>> GetCombos()
+        {
+            return await _readsvc.ReadDatas();
+        }
+
+        /// <summary>
+        /// Lấy thông tin combo theo comboCode
+        /// </summary>
+        /// <param name="code">comboCode</param>
+        /// <response name="404">Không tìm thấy</response>
+        /// <returns>Thông tin combo</returns>
+        [HttpGet("{code}")]
+        public async Task<ActionResult<Combo>> GetComboByCode(Guid code)
+        {
+            var data = await _lookupsvc.GetDataByKey(code);
+            if(data == null)
+            {
+                return NotFound();
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// Lấy thông tin combo theo comboName
+        /// </summary>
+        /// <param name="name">comboName</param>
+        /// <response name="404">Không tìm thấy</response>
+        /// <returns>Thông tin combo</returns>
+        [HttpGet("name/{name}")]
+        public async Task<ActionResult<IEnumerable<Combo>>> GetComboByName(string name)
+        {
+            var data = await _lookupsvc2.GetListByKey(name);
+            if(data == null)
+            {
+                return NotFound();
+            }
+            return data.ToList();
+        }
+
+        /// <summary>
+        /// Thêm một combo mới
+        /// </summary>
+        /// <example>
+        /// {
+        ///     "comboName": "Family Combo",
+        ///     "currentPrice: "100000",
+        ///     "image": "9304938273.jpg",
+        ///     "expDate: "30/08/2024 12:48:32 CH"
+        /// }
+        /// </example>
+        /// <response name="403">comboName đã được sử dụng</response>
+        /// <resposne name="201">Thành công</resposne>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> PostCombo([FromBody] Combo combo)
+        {
+            var data = await _addsvc.AddNewData(combo);
+            if(data == null)
+            {
+                return Forbid();
+            }
+            return Created();
+        }
+
+        /// <summary>
+        /// Sửa một combo được chọn theo comboCode
+        /// </summary>
+        /// <param name="code">comboCode</param>
+        /// <response name="404">Không tìm thấy</response>
+        /// <response name="202">Thành công</response>
+        /// <returns>Combo đã chỉnh sửa</returns>
+        [HttpPut("{code}")]
+        public async Task<IActionResult> PutCombo(Guid code, [FromBody] Combo combo)
+        {
+            if(code != combo.ComboCode)
+            {
+                return NotFound();
+            }
+            var data = await _editsvc.EditData(combo);
+            if (data == null)
+            {
+                return NotFound(); 
+            }
+            return Accepted(data);
+        }
+
+        /// <summary>
+        /// Xóa một combo
+        /// </summary>
+        /// <param name="code">comboCode</param>
+        /// <response name="404">Không tìm thấy</response>
+        /// <returns></returns>
+        [HttpDelete("{code}")]
+        public async Task<IActionResult> DeleteCombo(Guid code)
+        {
+            var data = await _deletesvc.DeleteData(code);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+    }
+}
--- API/Services/Implement/ComboSvc.cs
+++ API/Services/Implement/ComboSvc.cs
@@ -14,18 +14,6 @@ namespace API.Services.Implement
             _dbContext = dbContext;
         }
 
-        /// <summary>
-        /// Thêm một combo mới
-        /// </summary>
-        /// <example>
-        /// {
-        ///     "comboName": "Family Combo",
-        ///     "currentPrice: "100000",
-        ///     "image": "9304938273.jpg",
-        ///     "expDate: "30/08/2024 12:48:32 CH"
-        /// }
-        /// </example>
-        /// <returns></returns>
         public async Task<Combo> AddNewData(Combo entity)
         {
             var find = await _dbContext.combos.Where(x => x.ComboName.Contains(entity.ComboName, StringComparison.OrdinalIgnoreCase)).FirstOrDefaultAsync();
@@ -46,11 +34,6 @@ namespace API.Services.Implement
             return entity;
         }
 
-        /// <summary>
-        /// Xóa một combo
-        /// </summary>
-        /// <param name="key">comboCode</param>
-        /// <returns></returns>
         public async Task<string> DeleteData(Guid key)
         {
             var find = await _dbContext.combos.Where(x => x.ComboCode == key).FirstOrDefaultAsync();
@@ -63,10 +46,6 @@ namespace API.Services.Implement
             return $"Xóa {key} thành công";
         }
 
-        /// <summary>
-        /// Sửa một combo được chọn theo comboCode
-        /// </summary>
-        /// <returns>Combo đã chỉnh sửa</returns>
         public async Task<Combo> EditData(Combo entity)
         {
             var find = await _dbContext.combos.Where(x => x.ComboCode == entity.ComboCode).FirstOrDefaultAsync();
@@ -87,11 +66,6 @@ namespace API.Services.Implement
             return entity;
         }
 
-        /// <summary>
-        /// Lấy thông tin combo theo comboCode
-        /// </summary>
-        /// <param name="key">comboCode</param>
-        /// <returns>Thông tin combo</returns>
         public async Task<Combo> GetDataByKey(Guid key)
         {
             var find = await _dbContext.combos.Where(x => x.ComboCode == key).FirstOrDefaultAsync();
@@ -102,21 +76,12 @@ namespace API.Services.Implement
             return find;
         }
 
-        /// <summary>
-        /// Lấy thông tin combo theo comboName
-        /// </summary>
-        /// <param name="key">comboName</param>
-        /// <returns>Thông tin combo</returns>
         public async Task<IEnumerable<Combo>> GetListByKey(string key)
         {
             var find = await _dbContext.combos.Where(x => x.ComboName.Contains(key, StringComparison.OrdinalIgnoreCase)).ToListAsync();
             return find;
         }
 
-        /// <summary>
-        /// Lấy danh sách combo
-        /// </summary>
-        /// <returns>Danh sách combo</returns>
         public async Task<IEnumerable<Combo>> ReadDatas()
         {
             return await _dbContext.combos.ToListAsync();
