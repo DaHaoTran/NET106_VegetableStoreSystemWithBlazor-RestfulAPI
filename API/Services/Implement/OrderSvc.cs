a5097baec49a10fc8d323f3436b2a6cd20af0229
DaHaoTran <nhao4771@gmail.com>
2024-07-20 10:00:07 +0700

    Making OrderSvc

--- API/Services/Implement/OrderSvc.cs
+++ API/Services/Implement/OrderSvc.cs
@@ -1,67 +1,78 @@
 ﻿using API.Context;
-using UI.Models;
+using Models;
 using API.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using DTO;
 
 namespace API.Services.Implement
 {
-    public class OrderSvc : ILookupSvc<int, Order>, IEditable<Order>, IReadableHasWhere<string, Order>, IAddable<Order>
+    public class OrderSvc : IAddable<Order>, IDeletable<Guid, Order>, ILookupMoreSvc<string, Order>, IReadable<Order>
     {
-        private readonly FoodShopDBContext _dbContext;
-        public OrderSvc(FoodShopDBContext dbContext)
+        private readonly FastFoodDBContext _dbContext;
+        public OrderSvc(FastFoodDBContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-        public async Task<bool> AddNewData(Order entity)
+        /// <summary>
+        /// Thêm một đơn hàng mới
+        /// </summary>
+        /// <remarks>
+        /// Lưu ý:
+        /// Hãy có một customerInformation trong customerInformations (table database) trước khi thực hiện thêm mới này
+        /// Hãy có một customer trong customers (table database) trước khi thực hiện thêm mới này
+        /// </remarks>
+        /// <example>
+        /// {
+        ///     "state": "Not Delivered",
+        ///     "comment": "test data",
+        ///     "paymentMethod": "Thanh toán khi nhận hàng",
+        ///     "total": "200000",
+        ///     "cInforId": "..." (mã địa chỉ khách hàng),
+        ///     "customerEmail": "..." (email tài khoản khách hàng)
+        /// }
+        /// </example>
+        /// <returns></returns>
+        public async Task<Order> AddNewData(Order entity)
         {
-            try
+            Task t = Task.Run(() =>
             {
-                await _dbContext.order.AddAsync(entity);
-                await _dbContext.SaveChangesAsync();
-                return true;
-            } catch
+                entity.OrderCode = new Guid();
+                entity.OrderDate = DateTime.Now;
+            });
+            t.Wait();
+            await _dbContext.orders.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        /// <summary>
+        /// Xóa một đơn hàng 
+        /// </summary>
+        /// <param name="key">orderCode</param>
+        /// <returns></returns>
+        public async Task<string> DeleteData(Guid key)
+        {
+            var find = await _dbContext.orders.Where(x => x.OrderCode == key).FirstOrDefaultAsync();
+            if(find == default)
             {
-                return false;
+                return "Không tìm thấy";
             }
+            _dbContext.orders.Remove(find);
+            await _dbContext.SaveChangesAsync();
+            return $"Xóa {key} thành công !";
         }
 
-        public async Task<bool> EditData(Order entity)
+        /// <summary>
+        /// Lấy thông tin đơn hàng qua email
+        /// </summary>
+        /// <param name="key">email</param>
+        /// <returns>Thông tin đơn hàng</returns>
+        public async Task<IEnumerable<Order>> GetListByKey(string key)
         {
-            try
-            {
-                var find = await GetDataByKey(entity.OrderId);
-                find.State = entity.State;
-                if(find.State == "Delivered")
-                {
-                    find.DeliveryDate = DateTime.Now;
-                }
-                else
-                {
-                    find.DeliveryDate = null;
-                }
-                await _dbContext.SaveChangesAsync();
-                return true;
-            } catch
-            {
-                return false;
-            }
-        }
-
-        public Task<Order> GetDataByKey(int key)
-        {
-            return _dbContext.order.Where(x => x.OrderId == key).FirstOrDefaultAsync()!;
-        }
-
-        public Task<Order> GetDataByString(string str)
-        {
-            return _dbContext.order.Where(x => x.CustomerEmail == str.Trim()).FirstOrDefaultAsync()!;
-        }
-
-        public async Task<IEnumerable<Order>> ReadDatasHasW(string key) 
-        {
-            return await _dbContext.order.Where(x => x.State == key.Trim()).ToListAsync();
+            var find = await _dbContext.orders.Where(x => x.CustomerEmail == key).ToListAsync();
+            return find;
         }
     }
 }
--- Models/Order.cs
+++ Models/Order.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Models
@@ -6,26 +7,35 @@ namespace Models
     public class Order
     {
         [Key]
+        [DisplayName("Mã đơn")]
         public Guid OrderCode { get; set; }
 
+        [DisplayName("Ngày đặt")]
         public DateTime OrderDate { get; set; }
 
         [MaxLength(100)]
+        [DisplayName("Trạng thái")]
         public string State { get; set; }
 
+        [DisplayName("Ngày giao")]
         public DateTime? DeliveryDate { get; set; }
 
         [Column(TypeName = "Nvarchar(Max)")]
+        [DisplayName("Ghi chú")]
         public string? Comment { get; set; }
 
         [Column(TypeName = "Nvarchar(100)")]
+        [DisplayName("Phương thức thanh toán")]
         public string PaymentMethod { get; set; }
 
+        [DisplayName("Tổng tiền")]
         public int Total {  get; set; }
 
+        [DisplayName("Mã địa chỉ")]
         public int CInforId { get; set; }
 
         [Column(TypeName = "Varchar(200)")]
+        [DisplayName("Email khách hàng")]
         public string CustomerEmail { get; set; }
 
         [ForeignKey("CustomerEmail")]
