8bc61a3b8c97709027209d9cda99a9be70ecf3c3
DaHaoTran <nhao4771@gmail.com>
2024-07-21 15:25:18 +0700

    Making unit test API

--- XUnitTestAPI/API_FoodSvc.cs
+++ XUnitTestAPI/API_FoodSvc.cs
@@ -37,7 +37,7 @@ namespace XUnitTestAPI
             Customer customer = new Customer();
             customer.Email = "haotgps30117@fpt.edu.vn";
             customer.PassWord = "Giahao1";
-            _addsvc.Setup(x => x.AddNewData(customer));
+            _addsvc.Setup(x => x.AddNewData(customer)).ReturnsAsync(customer);
             _addsvc.Verify(x => x.AddNewData(customer), Times.Once);
         }
 
@@ -47,14 +47,15 @@ namespace XUnitTestAPI
             Customer customer = new Customer();
             customer.Email = "haotgps30117@fpt.edu.vn";
             customer.PassWord = "Giahao200";
-            _editsvc.Setup(x => x.EditData(customer));
+            _editsvc.Setup(x => x.EditData(customer)).ReturnsAsync(customer);
             _editsvc.Verify(x => x.EditData(customer), Times.Once);
         }
 
         [Fact(DisplayName = "CustomerSvc - GetList")]
         public void GetCustomers()
         {
-            _readsvc.Setup(x => x.ReadDatas());
+            List<Customer> customers = new List<Customer>();
+            _readsvc.Setup(x => x.ReadDatas()).ReturnsAsync(customers);
             _readsvc.Verify(x => x.ReadDatas(), Times.Once);
         }
 
@@ -62,7 +63,8 @@ namespace XUnitTestAPI
         [InlineData("haotgps30117@fpt.edu.vn")]
         public void GetCustomerByEmail(string email)
         {
-            _lookupsvc.Setup(x => x.GetDataByKey(email));
+            Customer customer = new Customer();
+            _lookupsvc.Setup(x => x.GetDataByKey(email)).ReturnsAsync(customer);
             _lookupsvc.Verify(x => x.GetDataByKey(email), Times.Once);
         }
 
@@ -70,8 +72,8 @@ namespace XUnitTestAPI
         [InlineData("haotgps30117@fpt.edu.vn")]
         public void DeleteCustomers(string email)
         {
-            //string ex = $"Xóa {email} thành công !";
-            _deletesvc.Setup(x => x.DeleteData(email));
+            string ex = $"Xóa {email} thành công !";
+            _deletesvc.Setup(x => x.DeleteData(email)).ReturnsAsync(ex);
             _deletesvc.Verify(x => x.DeleteData(email), Times.Once);
         }
     }
