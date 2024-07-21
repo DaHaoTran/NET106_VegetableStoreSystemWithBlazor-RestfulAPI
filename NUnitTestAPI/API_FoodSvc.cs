c3cc7af3833eecc5fe47871a8efd871914a5f8f9
DaHaoTran <nhao4771@gmail.com>
2024-07-21 11:21:47 +0700

    Making Test Svcs

---
+++ NUnitTestAPI/API_FoodSvc.cs
@@ -0,0 +1,66 @@
+﻿using API.Services.Implement;
+using DTO;
+using Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API;
+
+namespace XUnitTestAPI
+{
+    public class API_FoodSvc
+    {
+        private Bunit.TestContext testContext;
+        private Mock<CustomerSvc> loginSvc;
+
+        [SetUp]
+        public void Setup()
+        {
+            testContext = new Bunit.TestContext();
+            loginSvc = new Mock<CustomerSvc>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            testContext.Dispose();
+        }
+        public async void AddCustomer()
+        {
+            Customer customer = new Customer();
+            customer.Email = "haotgps30117@fpt.edu.vn";
+            customer.PassWord = "Giahao1";
+            Assert.NotNull(await _customerSvc.AddNewData(customer));
+        }
+
+        public async void EditCustomer()
+        {
+            Customer customer = new Customer();
+            customer.Email = "haotgps30117@fpt.edu.vn";
+            customer.PassWord = "Giahao200";
+            Assert.NotNull(await _customerSvc.EditData(customer));
+        }
+
+        public async void GetCustomers()
+        {
+            var customers = await _customerSvc.ReadDatas();
+            Assert.NotNull(customers);
+        }
+
+        [InlineData("haotgps30117@fpt.edu.vn")]
+        public async void GetCustomerByEmail(string email)
+        {
+            Assert.NotNull(await _customerSvc.GetDataByKey(email));
+        }
+
+        [InlineData("haotgps30117@fpt.edu.vn")]
+        public async void DeleteCustomers(string email)
+        {
+            string ex = $"Xóa {email} thành công !";
+            Assert.Equal(ex, await _customerSvc.DeleteData(email));
+        }
+    }
+}
