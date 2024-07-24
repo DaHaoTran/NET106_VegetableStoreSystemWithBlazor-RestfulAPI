476b63809063add9b1cae2d3762cc1db1f8c7b24
DaHaoTran <nhao4771@gmail.com>
2024-07-24 10:25:28 +0700

    Making CustomerSvc unit tests

--- XUnitTestAPI/API_FoodSvc.cs
+++ XUnitTestAPI/API_FoodSvc.cs
@@ -10,71 +10,74 @@ using API;
 using API.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using NUnit.Framework;
+using Xunit;
+using Microsoft.AspNetCore.SignalR;
 
 namespace XUnitTestAPI
 {
     public class API_FoodSvc
     {
-        private Mock<IAddable<Customer>> _addsvc;
-        private Mock<IEditable<Customer>> _editsvc;
-        private Mock<IDeletable<string, Customer>> _deletesvc;
-        private Mock<ILookupSvc<string, Customer>> _lookupsvc;
-        private Mock<IReadable<Customer>> _readsvc;
+        private CustomerSvc _customerSvc;
+        private FastFoodDBContext _dbContext;
+        private DbContextOptions<FastFoodDBContext> _options;
 
         public API_FoodSvc()
         {
-            _addsvc = new Mock<IAddable<Customer>>();
-            _editsvc = new Mock<IEditable<Customer>>();
-            _deletesvc = new Mock<IDeletable<string, Customer>>();
-            _lookupsvc = new Mock<ILookupSvc<string, Customer>>();
-            _readsvc = new Mock<IReadable<Customer>>();
+            _options = new DbContextOptionsBuilder<FastFoodDBContext>()
+            .UseInMemoryDatabase(databaseName: "NET106_ASM_FastFood")
+            .Options;
+            _dbContext = new FastFoodDBContext(_options);
+            if (_customerSvc == null) _customerSvc = new CustomerSvc(_dbContext);
         }
 
-        [Fact(DisplayName = "CustomerSvc - Add")]
-        public void AddCustomer()
+        [Fact(DisplayName = "CustomerSvc - Add sucessfully")]
+        public async void AddCustomerSucessfully()
         {
             Customer customer = new Customer();
             customer.Email = "haotgps30117@fpt.edu.vn";
             customer.PassWord = "Giahao1";
-            _addsvc.Setup(x => x.AddNewData(customer)).ReturnsAsync(customer);
-            _addsvc.Verify(x => x.AddNewData(customer), Times.Once);
+            //_addsvc.Setup(x => x.AddNewData(customer)).ReturnsAsync(customer);
+            //_addsvc.Verify(x => x.AddNewData(customer), Times.Once);
+            Assert.Equal(customer, await _customerSvc.AddNewData(customer));
         }
 
-        [Fact(DisplayName = "CustomerSvc - Edit")]
-        public void EditCustomer()
+        [Fact(DisplayName = "CustomerSvc - Edit sucessfully")]
+        public async void EditCustomerSucessfully()
         {
             Customer customer = new Customer();
             customer.Email = "haotgps30117@fpt.edu.vn";
             customer.PassWord = "Giahao200";
-            _editsvc.Setup(x => x.EditData(customer)).ReturnsAsync(customer);
-            _editsvc.Verify(x => x.EditData(customer), Times.Once);
+            //_editsvc.Setup(x => x.EditData(customer)).ReturnsAsync(customer);
+            //_editsvc.Verify(x => x.EditData(customer), Times.Once);
+            Assert.Equal(customer, await _customerSvc.EditData(customer));
         }
 
-        [Fact(DisplayName = "CustomerSvc - GetList")]
-        public void GetCustomers()
+        [Fact(DisplayName = "CustomerSvc - Get list")]
+        public async void GetCustomers()
         {
-            List<Customer> customers = new List<Customer>();
-            _readsvc.Setup(x => x.ReadDatas()).ReturnsAsync(customers);
-            _readsvc.Verify(x => x.ReadDatas(), Times.Once);
+            //_readsvc.Setup(x => x.ReadDatas()).ReturnsAsync(customers);
+            //_readsvc.Verify(x => x.ReadDatas(), Times.Once);
+            Assert.NotNull(await _customerSvc.ReadDatas());
         }
 
-        [Xunit.Theory(DisplayName = "CustomerSvc - GetList")]
+        [Xunit.Theory(DisplayName = "CustomerSvc - Get list by email")]
         [InlineData("haotgps30117@fpt.edu.vn")]
-        public void GetCustomerByEmail(string email)
+        public async void GetCustomerByEmail(string email)
         {
-            Customer customer = new Customer();
-            _lookupsvc.Setup(x => x.GetDataByKey(email)).ReturnsAsync(customer);
-            _lookupsvc.Verify(x => x.GetDataByKey(email), Times.Once);
+            //_lookupsvc.Setup(x => x.GetDataByKey(email)).ReturnsAsync(customer);
+            //_lookupsvc.Verify(x => x.GetDataByKey(email), Times.Once);
+            var customer = await _customerSvc.GetDataByKey(email);
+            Assert.Equal(email, customer.Email);
         }
 
-        [Xunit.Theory(DisplayName = "CustomerSvc - GetList")]
+        [Xunit.Theory(DisplayName = "CustomerSvc - Delete Sucessfully")]
         [InlineData("haotgps30117@fpt.edu.vn")]
-        public void DeleteCustomers(string email)
+        public async void DeleteCustomers(string email)
         {
             string ex = $"Xóa {email} thành công !";
-            _deletesvc.Setup(x => x.DeleteData(email)).ReturnsAsync(ex);
-            _deletesvc.Verify(x => x.DeleteData(email), Times.Once);
+            //_deletesvc.Setup(x => x.DeleteData(email)).ReturnsAsync(ex);
+            //_deletesvc.Verify(x => x.DeleteData(email), Times.Once);
+            Assert.Equal(ex, await _customerSvc.DeleteData(email));
         }
     }
 }
